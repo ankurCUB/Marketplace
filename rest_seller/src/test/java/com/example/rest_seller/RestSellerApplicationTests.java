c8528a4fa93cd43a3cd9a44bb575a84d4bf8b427
ankursharma <ansh2502@colorado.edu>
2023-02-21 19:50:53 -0700

    added rest module

---
+++ rest_seller/src/test/java/com/example/rest_seller/RestSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
